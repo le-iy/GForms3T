7c45e95b966eea0ff2315d63e0b38849054b2ebb
Le.i <220001650+le-iy@users.noreply.github.com>
2026-05-16 06:40:32 +0800

    Update data.js

--- data.js
+++ data.js
@@ -39,7 +39,7 @@ const subjects = [
             {
                 name: "Formatives",
                 icon: "📝",
-                description: "PHP formative activities.",
+                description: "Canvas-based: Formative assessments.",
                 items: [
                     {
                         title: "Formative 1",
@@ -57,19 +57,123 @@ const subjects = [
     },
 
     {
-        name: "Communication",
-        icon: "💬",
-        description: "Communication models, noise, and paralanguage.",
+        name: "GED0081",
+        icon: "⚛️",
+        description: "Physics 1",
+        categories: [
+            {
+                name: "LEC: Formatives",
+                icon: "📝",
+                description: "Hell.",
+                items: [
+                    {
+                        title: "Formative 1",
+                        description: "Introduction to PHP.",
+                        link: "https://forms.cloud.microsoft/r/xSTHYiVjV0"
+                    }
+                ]
+            },
+
+            {
+                name: "LAB: Short Quizzes",
+                icon: "📝",
+                description: "Canvas-based Short Quizzes.",
+                items: [
+                    {
+                        title: "Short Quiz 1",
+                        description: "Introduction to PHP.",
+                        link: "https://forms.cloud.microsoft/r/ffz0iNdSj0"
+                    }
+                ]
+            },
+
+            {
+                name: "LEC: PRETEST",
+                icon: "📝",
+                description: "Eme ni sir.",
+                items: [
+                    {
+                        title: "Short Quiz 1",
+                        description: "Introduction to PHP.",
+                        link: "https://forms.cloud.microsoft/r/q1p4Bw8cG3"
+                    }
+                ]
+            }
+        ]
+    },
+
+    {
+        name: "IT0203",
+        icon: "💻",
+        description: "Digital Forensics",
         categories: [
             {
                 name: "Formatives",
                 icon: "📝",
-                description: "Communication formative links.",
+                description: "Canvas-based: Formative assessments.",
                 items: [
                     {
                         title: "Formative 1",
-                        description: "Communication process.",
-                        link: "https://forms.gle/PASTE_LINK_HERE"
+                        description: "Introduction to PHP.",
+                        link: "https://forms.cloud.microsoft/r/zhCKdRy5Va"
+                    }
+                ]
+            }
+        ]
+    },
+
+    {
+        name: "IT0203",
+        icon: "💻",
+        description: "Digital Forensics",
+        categories: [
+            {
+                name: "Formatives",
+                icon: "📝",
+                description: "Canvas-based: Formative assessments.",
+                items: [
+                    {
+                        title: "Formative 1",
+                        description: "Introduction to PHP.",
+                        link: "https://forms.cloud.microsoft/r/zhCKdRy5Va"
+                    }
+                ]
+            }
+        ]
+    },
+
+    {
+        name: "IT0202",
+        icon: "💻",
+        description: "Ethical Hacking",
+        categories: [
+            {
+                name: "Netacad",
+                icon: "📝",
+                description: "Netacad Practical and Summary Quizzes",
+                items: [
+                    {
+                        title: "Module 1",
+                        description: "Introduction to Ethical Hacking and Penetration Testing.",
+                        link: "https://forms.cloud.microsoft/r/76PxpVAPYR"
+                    },
+                    {
+                        title: "Module 2",
+                        description: "Planning and Scoping a Penetration Testing Assessment.",
+                        link: "https://forms.cloud.microsoft/r/kKz4Z0TS74"
+                    }
+                ]
+            },
+
+            {
+                name: "Formatives",
+                icon: "📝",
+                description: "Canvas-based: Formative assessments.",
+                items: [
+                    {
+                        title: "Formative 1",
+                        description: "Introduction to PHP.",
+                        link: "https://forms.cloud.microsoft/r/myUu9q7Fxy"
                     }
                 ]
             }
