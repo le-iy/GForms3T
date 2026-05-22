722755f573381e5be6fc3ccf6749ed521d540be1
Le.i <220001650+le-iy@users.noreply.github.com>
2026-05-22 20:59:10 +0800

    Update data.js

--- data.js
+++ data.js
@@ -71,7 +71,19 @@ const subjects = [
                         title: "Formative 2",
                         description: "Control Structures.",
                         link: "https://forms.cloud.microsoft/r/eeqnr2GcXn",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMDZTM1ZLSVVRU0ZNS0g4R1FFOUVBQ0c2Ri4u&Token=3c79b1d657cb4600b0af30950240b487"
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUM0lBRlpQQlhLQUlHVzhESTRIOTg3N003Ty4u&Token=bc18f649349b4cf5ab35be9c7d7010ab"
+                    },
+                    {
+                        title: "Formative 3",
+                        description: "Arrays and User-defined Functions.",
+                        link: "https://forms.office.com/r/9wH1MA2Xyt",
+                        collabLink: "https://forms.office.com/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EGVakDeiDU9BujlyMTLO6NpUQkhUNlpZSzFKMFJSQzlOQUNaRzBPMEE0Wi4u&Token=3df165ac1b0a4c8fbac21d618c5fb549"
+                    },
+                    {
+                        title: "Formative 4",
+                        description: "Predefined Functions.",
+                        link: "https://forms.office.com/r/xtYi4VEzi9",
+                        collabLink: "https://forms.office.com/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EGVakDeiDU9BujlyMTLO6NpUMUVWRFlBNzQ1SVZYWjdCQjROSzhINVBGRi4u&Token=5a9e4714c003472b8ac42d288af0b949"
                     }
                 ]
             }
