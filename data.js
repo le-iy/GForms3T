bec2f05b109a99f18f1e44ee947eefe9d4134bcd
Le.i <220001650+le-iy@users.noreply.github.com>
2026-05-31 16:50:48 +0800

    Update data.js

--- data.js
+++ data.js
@@ -285,32 +285,13 @@ const subjects = [
                         title: "Formative 1",
                         description: "Ethical Hacking formative quiz.",
                         link: "https://forms.cloud.microsoft/r/myUu9q7Fxy",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUNVpQRTc4S0dXOUpDR1M2T1dMVTNRTllTSy4u&Token=a39a7f3d4ec94424b05c8c45ae629443",
-                        repositories: [
-                            {
-                                title: "Canvas Module 1",
-                                link: "File_Bank/IT0202/Module 1 - Computer Forensics Fundamentals (Revised) (2).pdf"
-                            },
-                            {
-                                title: "Canvas Module 2",
-                                link: "File_Bank/IT0202/Module 2 - Computer Forensics Investigation Process (Revised) (2).pdf"
-                            }
-                        ]
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUNVpQRTc4S0dXOUpDR1M2T1dMVTNRTllTSy4u&Token=a39a7f3d4ec94424b05c8c45ae629443"
                     },
                     {
                         title: "Formative 2",
                         description: "Ethical Hacking formative quiz.",
                         link: "https://forms.cloud.microsoft/r/7y04JbLJZf",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMlAyWUxSWFNFR1dUQlA2MjU0OEo0NlJKMC4u&Token=3bbbe732da3541749a63c37d07b9a923",
-                        repositories: [
-                            {
-                                title: "Canvas Module 3",
-                                link: "File_Bank/IT0202/Module 3 - Understanding Hard Disks and File Systems (Revised) (1).pdf"
-                            },
-                            {
-                                title: "Canvas Module 4",
-                                link: "File_Bank/IT0202/Module 4 - Data Acquisition and Duplication - Defeating Anti-Forensic Techniques (Revised) (1).pdf"}
-                        ]
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMlAyWUxSWFNFR1dUQlA2MjU0OEo0NlJKMC4u&Token=3bbbe732da3541749a63c37d07b9a923"
                     }
                 ]
             }
@@ -331,13 +312,32 @@ const subjects = [
                         title: "Formative 1",
                         description: "Digital Forensics formative quiz.",
                         link: "https://forms.cloud.microsoft/r/zhCKdRy5Va",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxURFZKRFJLQTVOQlpIM0VPQkdJTVY1TlhOSi4u&Token=3a62e81c999b4c2cad5313c787035b29"
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxURFZKRFJLQTVOQlpIM0VPQkdJTVY1TlhOSi4u&Token=3a62e81c999b4c2cad5313c787035b29",
+                        repositories: [
+                            {
+                                title: "Canvas Module 1",
+                                link: "File_Bank/IT0202/Module 1 - Computer Forensics Fundamentals (Revised) (2).pdf"
+                            },
+                            {
+                                title: "Canvas Module 2",
+                                link: "File_Bank/IT0202/Module 2 - Computer Forensics Investigation Process (Revised) (2).pdf"
+                            }
+                        ]
                     },
                     {
                         title: "Formative 2",
                         description: "Digital Forensics formative quiz.",
                         link: "https://forms.cloud.microsoft/r/zQqK44WjFv",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EAXZJxttnntDsUKyJ6bK5bZUMzM0WVdRNFVPVExWNlI1OVk0WkpSSlhLTS4u&Token=cfeb8fe789a7432b87494f69fd879a71"
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EAXZJxttnntDsUKyJ6bK5bZUMzM0WVdRNFVPVExWNlI1OVk0WkpSSlhLTS4u&Token=cfeb8fe789a7432b87494f69fd879a71",
+                        repositories: [
+                            {
+                                title: "Canvas Module 3",
+                                link: "File_Bank/IT0202/Module 3 - Understanding Hard Disks and File Systems (Revised) (1).pdf"
+                            },
+                            {
+                                title: "Canvas Module 4",
+                                link: "File_Bank/IT0202/Module 4 - Data Acquisition and Duplication - Defeating Anti-Forensic Techniques (Revised) (1).pdf"}
+                        ]
                     }
                 ]
             }
