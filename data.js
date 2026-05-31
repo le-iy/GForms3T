bf9207f9d8fb8e152d819fcd73f2525c733e34f8
Le.i <220001650+le-iy@users.noreply.github.com>
2026-05-31 15:51:47 +0800

    Update data.js

--- data.js
+++ data.js
@@ -224,31 +224,81 @@ const subjects = [
                         title: "Module 1",
                         description: "Introduction to Ethical Hacking and Penetration Testing.",
                         link: "https://forms.cloud.microsoft/r/76PxpVAPYR",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMzdZUTVBTE1ESExIQ0o0N1c0RDhPRjNOVS4u&Token=d5a5665ab73e4955bb82e9d6ab85235d"
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMzdZUTVBTE1ESExIQ0o0N1c0RDhPRjNOVS4u&Token=d5a5665ab73e4955bb82e9d6ab85235d",
+                        repositories: [
+                            {
+                                title: "Ethical Hacking Midterm Reviewer",
+                                link: "File_Bank/IT0202/Netacad M1.pdf"
+                            },
+                            {
+                                title: "Module 1 Source PDF",
+                                link: "File_Bank/IT0202/Netacad M1.pdf"
+                            }
+                        ]
                     },
                     {
                         title: "Module 2",
                         description: "Planning and Scoping a Penetration Testing Assessment.",
                         link: "https://forms.cloud.microsoft/r/kKz4Z0TS74",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUN01ESTZMUVpYNjJMQzVMQlkxNU9aVzRQRS4u&Token=b4fa8a30865c4ba2928fb6d008612122"
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUN01ESTZMUVpYNjJMQzVMQlkxNU9aVzRQRS4u&Token=b4fa8a30865c4ba2928fb6d008612122",
+                        repositories: [
+                            {
+                                title: "Ethical Hacking Midterm Reviewer",
+                                link: "File_Bank/IT0202/Netacad M1.pdf"
+                            },
+                            {
+                                title: "Module 2 Source PDF",
+                                link: "File_Bank/IT0202/Netacad M2.pdf"
+                            }
+                        ]
                     },
                     {
                         title: "Module 3",
                         description: "Information Gathering and Vulnerability Scanning.",
                         link: "https://forms.cloud.microsoft/r/D1MAdRCSVV",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMjBQR1o5NEsyUkZUWjFENllWUlc4WU5PRi4u&Token=8c8ffb2a02324a60b50051f86df74fe5"
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMjBQR1o5NEsyUkZUWjFENllWUlc4WU5PRi4u&Token=8c8ffb2a02324a60b50051f86df74fe5",
+                        repositories: [
+                            {
+                                title: "Ethical Hacking Midterm Reviewer",
+                                link: "File_Bank/IT0202/Netacad M1.pdf"
+                            },
+                            {
+                                title: "Module 3 Source PDF",
+                                link: "File_Bank/IT0202/Netacad M3.pdf"
+                            }
+                        ]
                     },
                     {
                         title: "Module 4",
                         description: "Social Engineering Attacks.",
                         link: "https://forms.cloud.microsoft/r/TWsW554u4u",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMUtPUlAwRDVSWVpJTU1BMFlJWjlEVUFLTi4u&Token=a092688b06084ce8afdcb98f766e6387"
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMUtPUlAwRDVSWVpJTU1BMFlJWjlEVUFLTi4u&Token=a092688b06084ce8afdcb98f766e6387",
+                        repositories: [
+                            {
+                                title: "Ethical Hacking Midterm Reviewer",
+                                link: "File_Bank/IT0202/Netacad M1.pdf"
+                            },
+                            {
+                                title: "Module 4 Source PDF",
+                                link: "File_Bank/IT0202/Netacad M4.pdf"
+                            }
+                        ]
                     },
                     {
                         title: "Module 5",
                         description: "Exploiting Wired and Wireless Networks.",
                         link: "https://forms.cloud.microsoft/r/EEcT29JLae",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUQUpBREtJTU4xUUxEQUZJRVU3ODY4TzZZVS4u&Token=b35ca10ed17949ddbc34c885dcdc8e55"
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUQUpBREtJTU4xUUxEQUZJRVU3ODY4TzZZVS4u&Token=b35ca10ed17949ddbc34c885dcdc8e55",
+                        repositories: [
+                            {
+                                title: "Ethical Hacking Midterm Reviewer",
+                                link: "File_Bank/IT0202/Netacad M1.pdf"
+                            },
+                            {
+                                title: "Module 5 Source PDF",
+                                link: "File_Bank/IT0202/Netacad M5.pdf"
+                            }
+                        ]
                     }
                 ]
             },
