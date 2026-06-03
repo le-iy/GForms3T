b03eaf6beca20fba9fb5bc729ccea3b14a92cc9e
Le.i <220001650+le-iy@users.noreply.github.com>
2026-06-03 11:20:42 +0800

    Update data.js

--- data.js
+++ data.js
@@ -32,19 +32,37 @@ const subjects = [
                         title: "Quiz 1 Prep",
                         description: "Module-based generated Q1 prep.",
                         link: "https://forms.cloud.microsoft/r/sFBma6TMCW",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUNE9YRTFYQTUwMEJDSVJVWUNZMzQ0RVVVSC4u&Token=f078d5fcf9844b95aae162834cf64c42"
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUNE9YRTFYQTUwMEJDSVJVWUNZMzQ0RVVVSC4u&Token=f078d5fcf9844b95aae162834cf64c42",
+                        repositories: [
+                                    {
+                                        title: "Module 1 Canvas PDF",
+                                        link: "File_Bank/GED0081/[M1-MAIN] Introduction to Communication-2.pdf"
+                                    }
+                        ]
                     },
                     {
                         title: "Quiz 2 [S1] Prep",
                         description: "Module-based generated Q2 Subtopic 1 prep.",
                         link: "https://forms.cloud.microsoft/r/6GqBcJG2aU",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMlYwSFlCV0g2OUFYRUFKRDNKQVRDMFk2Sy4u&Token=5d2541223e6d4265bf36569be7f96b82"
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMlYwSFlCV0g2OUFYRUFKRDNKQVRDMFk2Sy4u&Token=5d2541223e6d4265bf36569be7f96b82",
+                        repositories: [
+                                    {
+                                        title: "Module 2 S1 Canvas PDF",
+                                        link: "File_Bank/GED0081/[M2S1-POWERPOINT] Intellectual Property-1 (1).pdf"
+                                    }
+                        ]
                     },
                     {
                         title: "Quiz 2 [S2] Prep",
                         description: "Module-based generated Q2 Subtopic 2 prep.",
                         link: "https://forms.cloud.microsoft/r/xB41QU686x",
-                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUM0lJWFNJT1FPM0IzNkNDWUdXTU1UWkEzMy4u&Token=ec9204f757984ec6acc8162db7cee5ae"
+                        collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUM0lJWFNJT1FPM0IzNkNDWUdXTU1UWkEzMy4u&Token=ec9204f757984ec6acc8162db7cee5ae",
+                        repositories: [
+                                    {
+                                        title: "Module 2 S2 Canvas PDF",
+                                        link: "File_Bank/GED0081/[M2S2-POWERPOINT] Ethics in Communication-1 (1).pdf"
+                                    }
+                        ]
                     }
                 ]
             }
