2baa66897f145f3367fba1349556d6b32b3973f6
Le.i <220001650+le-iy@users.noreply.github.com>
2026-06-05 00:01:30 +0800

    Create it0203.js

---
+++ subjects/it0203.js
@@ -0,0 +1,46 @@
+const IT0203 = {
+    name: "IT0203",
+    icon: "🔎💻",
+    description: "Digital Forensics",
+    categories: [
+        {
+            name: "Formatives",
+            icon: "📝",
+            description: "Canvas-based: Formative assessments.",
+            items: [
+                {
+                    title: "Formative 1",
+                    description: "Digital Forensics formative quiz.",
+                    link: "https://forms.cloud.microsoft/r/zhCKdRy5Va",
+                    collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxURFZKRFJLQTVOQlpIM0VPQkdJTVY1TlhOSi4u&Token=3a62e81c999b4c2cad5313c787035b29",
+                    repositories: [
+                        {
+                            title: "Canvas Module 1",
+                            link: "File_Bank/IT0203/Module 1 - Computer Forensics Fundamentals (Revised) (2).pdf"
+                        },
+                        {
+                            title: "Canvas Module 2",
+                            link: "File_Bank/IT0203/Module 2 - Computer Forensics Investigation Process (Revised) (2).pdf"
+                        }
+                    ]
+                },
+                {
+                    title: "Formative 2",
+                    description: "Digital Forensics formative quiz.",
+                    link: "https://forms.cloud.microsoft/r/zQqK44WjFv",
+                    collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EAXZJxttnntDsUKyJ6bK5bZUMzM0WVdRNFVPVExWNlI1OVk0WkpSSlhLTS4u&Token=cfeb8fe789a7432b87494f69fd879a71",
+                    repositories: [
+                        {
+                            title: "Canvas Module 3",
+                            link: "File_Bank/IT0203/Module 3 - Understanding Hard Disks and File Systems (Revised) (1).pdf"
+                        },
+                        {
+                            title: "Canvas Module 4",
+                            link: "File_Bank/IT0203/Module 4 - Data Acquisition and Duplication - Defeating Anti-Forensic Techniques (Revised) (1).pdf"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
