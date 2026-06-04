20dc1681883c392d61d8663638791e9c691e6526
Le.i <220001650+le-iy@users.noreply.github.com>
2026-06-05 00:01:36 +0800

    Create ged0031.js

---
+++ subjects/ged0031.js
@@ -0,0 +1,69 @@
+const GED0031 = {
+    name: "GED0031",
+    icon: "📚🗣",
+    description: "Purposive Communication",
+    categories: [
+        {
+            name: "Formatives",
+            icon: "📝",
+            description: "Canvas-based: Formative assessments.",
+            items: [
+                {
+                    title: "Formative 1",
+                    description: "Module 1 formative quiz.",
+                    link: "https://forms.cloud.microsoft/r/zsvLhD2qfx",
+                    collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUQVc0WVNDVFJCTzRDSlYzR1RDUklXWk1DQi4u&Token=0d58cf76bf824ecb9c947ee6f62708fc"
+                },
+                {
+                    title: "Formative 2",
+                    description: "Module 2 formative quiz.",
+                    link: "https://forms.cloud.microsoft/r/uqYK9NUmgw",
+                    collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUM0xEUU9KR1RGUEE3WFVLS0o0UUU2OE42RC4u&Token=a490ab495f1949229bd4ba496ff696cf"
+                }
+            ]
+        },
+        {
+            name: "Practice Quizzes",
+            icon: "📚",
+            description: "Module-generated quizzes.",
+            items: [
+                {
+                    title: "Quiz 1 Prep",
+                    description: "Module-based generated Q1 prep.",
+                    link: "https://forms.cloud.microsoft/r/sFBma6TMCW",
+                    collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUNE9YRTFYQTUwMEJDSVJVWUNZMzQ0RVVVSC4u&Token=f078d5fcf9844b95aae162834cf64c42",
+                    repositories: [
+                        {
+                            title: "Module 1 Canvas PDF",
+                            link: "File_Bank/GED0031/M1.pdf"
+                        }
+                    ]
+                },
+                {
+                    title: "Quiz 2 [S1] Prep",
+                    description: "Module-based generated Q2 Subtopic 1 prep.",
+                    link: "https://forms.cloud.microsoft/r/6GqBcJG2aU",
+                    collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMlYwSFlCV0g2OUFYRUFKRDNKQVRDMFk2Sy4u&Token=5d2541223e6d4265bf36569be7f96b82",
+                    repositories: [
+                        {
+                            title: "Module 2 S1 Canvas PDF",
+                            link: "File_Bank/GED0031/M2S1.pdf"
+                        }
+                    ]
+                },
+                {
+                    title: "Quiz 2 [S2] Prep",
+                    description: "Module-based generated Q2 Subtopic 2 prep.",
+                    link: "https://forms.cloud.microsoft/r/xB41QU686x",
+                    collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUM0lJWFNJT1FPM0IzNkNDWUdXTU1UWkEzMy4u&Token=ec9204f757984ec6acc8162db7cee5ae",
+                    repositories: [
+                        {
+                            title: "Module 2 S2 Canvas PDF",
+                            link: "File_Bank/GED0031/M2S2.pdf"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
