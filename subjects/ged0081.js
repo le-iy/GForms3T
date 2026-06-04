d6f76c6b3c27340fd3fe2f22389501f793fc520c
Le.i <220001650+le-iy@users.noreply.github.com>
2026-06-05 00:01:34 +0800

    Create ged0081.js

---
+++ subjects/ged0081.js
@@ -0,0 +1,144 @@
+const GED0081 = {
+    name: "GED0081",
+    icon: "⚛️",
+    description: "Physics 1",
+    categories: [
+        {
+            name: "LEC",
+            icon: "📖",
+            description: "Lecture-based quizzes and pretests.",
+            categories: [
+                {
+                    name: "Formatives",
+                    icon: "📝",
+                    description: "Lecture formative assessments.",
+                    items: [
+                        {
+                            title: "Formative 1",
+                            description: "Lecture formative quiz.",
+                            link: "https://forms.cloud.microsoft/r/xSTHYiVjV0",
+                            collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUQ01EQkNQNjgxT1g5SkE1SE5ETDNIOU1QTy4u&Token=3ef439a4dcce4f06b52337c84014edd1"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: "LAB",
+            icon: "🧪",
+            description: "Laboratory quizzes and module activity reviewers.",
+            categories: [
+                {
+                    name: "Short Quizzes (Laboratory)",
+                    icon: "🧪",
+                    description: "...",
+                    items: [
+                        {
+                            title: "Short Quiz 1",
+                            description: "Lab short quiz.",
+                            link: "https://forms.cloud.microsoft/r/x1JvYctfBN",
+                            collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUOTFBRE02TkUxVllGODdYSkZZUlpFWFFIMC4u&Token=8e28ae3611a54090a39196882e3c24c4",
+                            repositories: [
+                                {
+                                    title: "Lab 1 Quiz",
+                                    link: "File_Bank/GED0081/ShortQuiz1.pdf"
+                                }
+                            ]
+                        },
+                        {
+                            title: "Short Quiz 2",
+                            description: "Lab short quiz.",
+                            link: "https://forms.cloud.microsoft/r/Shx09zbxVC",
+                            collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUREU3MDZMRThETUwzN1U5UEUzRVNCWE1RVC4u&Token=a6c3d34f063c48c782ce636b9cda6183",
+                            repositories: [
+                                {
+                                    title: "Lab 2 Quiz",
+                                    link: "File_Bank/GED0081/ShortQuiz2.pdf"
+                                }
+                            ]
+                        },
+                        {
+                            title: "Short Quiz 3",
+                            description: "Lab short quiz.",
+                            link: "https://forms.cloud.microsoft/r/D02S5vFdV0",
+                            collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUMFQ0T0dWVjZKM1lYOFVJRUdJMlBUUkg1QS4u&Token=174bd37b29f64a74858270c9af8f8c15",
+                            repositories: [
+                                {
+                                    title: "Lab 3 Quiz",
+                                    link: "File_Bank/GED0081/ShortQuiz3.pdf"
+                                }
+                            ]
+                        },
+                        {
+                            title: "Short Quiz 4",
+                            description: "Lab short quiz.",
+                            link: "https://forms.cloud.microsoft/r/s008zx2x0E",
+                            collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUOVBHWlFDU1oyTEEwTjFQUE9IUVo5SElZSC4u&Token=30c98b31ef114b30a6b8ed23959bb014",
+                            repositories: [
+                                {
+                                    title: "Lab 4 Quiz",
+                                    link: "File_Bank/GED0081/ShortQuiz4.pdf"
+                                }
+                            ]
+                        },
+                        {
+                            title: "Short Quiz 5",
+                            description: "Lab short quiz.",
+                            link: "https://forms.cloud.microsoft/r/W75WYBU1j7",
+                            collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUOEJHRTJBUjZaNDlXVFc2SzBTOUQwVzNDTy4u&Token=0b123905610e4463a3a59739aa7c09f4",
+                            repositories: [
+                                {
+                                    title: "Lab 5 Quiz",
+                                    link: "File_Bank/GED0081/ShortQuiz5.pdf"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    name: "Module Activity Practice Reviewers",
+                    icon: "🔬",
+                    description: "Module activity-based practice questions.",
+                    items: [
+                        {
+                            title: "Module 3",
+                            description: "Module 3 activity reviewer.",
+                            link: "https://forms.cloud.microsoft/r/JKifvH7tCE",
+                            collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUNURQTDg2N0dGOEVDU01VVzFIOVA4REdMMi4u&Token=165988a5cf3e4e3785825649b362b512",
+                            repositories: [
+                                {
+                                    title: "Module 3 Source PDF",
+                                    link: "File_Bank/GED0081/Activity Lab 3.pdf"
+                                }
+                            ]
+                        },
+                        {
+                            title: "Module 4",
+                            description: "Module 4 activity reviewer.",
+                            link: "https://forms.cloud.microsoft/r/VveRqdGh45",
+                            collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxUNEgyQloyVkNFVlJPWlFFNzNRUjJJVllZSy4u&Token=8928765b897f45b1a885084102444c47",
+                            repositories: [
+                                {
+                                    title: "Module 4 Source PDF",
+                                    link: "File_Bank/GED0081/Activity Lab 4.pdf"
+                                }
+                            ]
+                        },
+                        {
+                            title: "Module 5",
+                            description: "Module 5 activity reviewer.",
+                            link: "https://forms.cloud.microsoft/r/9VLqdT5PLQ",
+                            collabLink: "https://forms.cloud.microsoft/Pages/DesignPageV2.aspx?subpage=design&FormId=2SWgsIjLCESbFc531Hw4EK1ypcd9WIhGl16-VZ5DfUxURDZQRU1aTVhSV0NMNU1NVTk0NFZUM0hQRC4u&Token=157a9bea50584eed94c3aab7ac3b6c09",
+                            repositories: [
+                                {
+                                    title: "Module 5 Source PDF",
+                                    link: "File_Bank/GED0081/Activity Lab 5.pdf"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
