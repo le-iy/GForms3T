980e1522414b0a64226f5d354cca77e3989960f9
Le.i <220001650+le-iy@users.noreply.github.com>
2026-06-05 20:32:29 +0800

    Update extras.js

--- extras.js
+++ extras.js
@@ -3,5 +3,10 @@ const extraLinks = [
         title: "CCS0043 - PHP Sequential Interactive Reviewer",
         type: "GitHub",
         link: "https://prinsipekazu.github.io/practice.repo/"
+    },
+    {
+        title: "CCS0043 - Summative 1 Prep",
+        type: "Gizmo",
+        link: "https://gizmo.ai/deck/78235393?p=DdtkiQUFFzQ&t=1&featureId=2&deckName=Summative+1+Prep+%28Generated+Module-based%29&deckColour=818cf8&senderName=Nei&numDeckCards=120&senderUserId=12419649"
     }
 ];
