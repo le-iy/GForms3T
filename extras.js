9fafcd9ff4710cbb5c2a9309503984b3f4f5cebe
Le.i <220001650+le-iy@users.noreply.github.com>
2026-06-06 04:10:39 +0800

    Update extras.js

--- extras.js
+++ extras.js
@@ -10,5 +10,11 @@ const extraLinks = [
         description: "Summative 1 Prep",
         type: "Gizmo",
         link: "https://gizmo.ai/deck/78235393?p=DdtkiQUFFzQ&t=1&featureId=2&deckName=Summative+1+Prep+%28Generated+Module-based%29&deckColour=818cf8&senderName=Nei&numDeckCards=120&senderUserId=12419649"
+    },
+    {
+        title: "GED0031",
+        description: "Summative 1 / ME Prep",
+        type: "Gizmo",
+        link: "https://gizmo.ai/deck/84857924?p=wPrc0q4kO_w&t=1&featureId=2&deckName=Summative+1&deckColour=fbbf24&senderName=Nei&numDeckCards=49&senderUserId=12419649"
     }
 ];
