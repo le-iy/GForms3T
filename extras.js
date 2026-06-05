88b47b5be7a01098c63ce57befc824db3d54ecbd
Le.i <220001650+le-iy@users.noreply.github.com>
2026-06-05 20:06:00 +0800

    Create extras.js

---
+++ extras.js
@@ -0,0 +1,7 @@
+const extraLinks = [
+    {
+        title: "CCS0043 - PHP Sequential Interactive Reviewer",
+        type: "GitHub",
+        link: "https://prinsipekazu.github.io/practice.repo/"
+    }
+];
